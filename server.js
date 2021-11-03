93710baac36103da2ceb50f7e0f3bc0b5ea253f0
Irina Plaksina <iplaksina@vigilantioe.com>
2021-11-03 16:57:23 -0400

    compare Es5 and Es6 scopes

--- server.js
+++ server.js
@@ -95,10 +95,72 @@ function test() {
 
 test();
 
-let a = [1,2,3];
-let b = [1,2,3];
-let c = "1,2,3";
+let a = [1, 2, 3];
+let b = [1, 2, 3];
+const c = "1,2,3";
 
-console.log(`${a==c}\n${b==c}\n${a==b}`);
+console.log(`${a == c}\n${b == c}\n${a == b}`);
 
+// scopes
 
+function mooEs5() {
+    var aEs5 = 1;
+
+    function barEs5() {
+        var bEs5 = 2;
+
+        if (bEs5 < 3) {
+            var doubleB = bEs5 * 2
+            console.log(`doubleB: ${doubleB}`);
+        }
+
+        function bazEs5() {
+            var cEs5 = 3;
+            dEs5 = 4;
+            console.log(`bazEs5: AEs5: ${aEs5}, BEs5: ${bEs5}, CEs5: ${cEs5}, DEs5: ${dEs5}`);
+            console.log(`ES5 code block declaration: ${doubleB}`);
+        }
+
+        bazEs5();
+        console.log(`barEs5: AEs5: ${aEs5}, BEs5: ${bEs5}`);
+        // cEs5, dEs5 are not defined
+    }
+
+    barEs5();
+    console.log(`mooEs5: AEs5: ${aEs5}`);
+    // bEs5, cEs5, dEs5 are not defined
+}
+
+mooEs5();
+console.log(`global: DEs5: ${dEs5}`);
+
+function mooEs6() {
+    const aEs6 = 1;
+
+    function barEs6() {
+        const bEs6 = 2;
+
+        if (bEs6 < 3) {
+            const doubleBEs6 = bEs6 * 2
+            console.log(`doubleBEs6: ${doubleBEs6}`);
+        }
+
+        function bazEs6() {
+            const cEs6 = 3;
+            dEs6 = 4;
+            console.log(`baz: AEs6: ${aEs6}, BEs6: ${bEs6}, CEs6: ${cEs6}, DEs6: ${dEs6}`);
+            // console.log(`ES6 code block declaration: ${doubleBEs6}`); // doubleBes6 is not defined
+        }
+
+        bazEs6();
+        console.log(`barEs6: AEs6: ${aEs6}, BEs6: ${bEs6}`);
+        // cEs6, dEs6 are not defined
+    }
+
+    barEs6();
+    console.log(`mooEs6: AEs6: ${aEs6}`);
+    // bEs6, cEs6, dEs6 are not defined
+}
+
+mooEs6();
+console.log(`global: DEs6: ${dEs6}`);
